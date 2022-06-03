0beb65def302267eaee57315ea90583c53773225
Yury Kosyakov <ykosyakov@gmail.com>
2022-06-03 12:29:44 +0300

    Dummy test for XmlMerger

--- test/xml-merger.spec.ts
+++ test/xml-merger.spec.ts
@@ -1,1 +1,7 @@
-describe('XmlMerger', () => {});
+import { XmlMerger } from '../src/xml-merge/xml-merger';
+
+describe('XmlMerger', () => {
+	it('Dummy', () => {
+		const merger = new XmlMerger();
+	});
+});
